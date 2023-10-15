bf9d88a99da8fc17a720c6e8cf2c59118173488f
FlavioOlivieri <flavio.olivieri2002@gmail.com>
2023-10-15 11:54:52 +0200

    manca animazione e ricarica

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,26 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
-
-
 import { PrimengModule } from 'src/primeng.module';
 
-import { LoginComponent } from './login/login.component';
+import { LoginComponent} from './login/login.component';
 import { GlobeComponent } from './globe/globe.component';
+import { RegisterComponent } from './register/register.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
-    GlobeComponent
+    GlobeComponent,
+    RegisterComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    PrimengModule
-
+    PrimengModule,
   ],
   providers: [
   ],
