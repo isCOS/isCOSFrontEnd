5686c4823490fc692f5165b66c6bc3fd3f26d599
FlavioOlivieri <flavio.olivieri2002@gmail.com>
2023-10-19 17:39:35 +0200

    registration almost fix

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -3,17 +3,37 @@ import { Output, EventEmitter } from '@angular/core';
 import { FormControl, FormBuilder, Validators } from '@angular/forms';
 
 
+interface driveLicences {
+  name: string;
+  code: string
+}
+
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.scss'],
 })
 export class RegisterComponent implements OnInit {
+  driveLicences: driveLicences[] | undefined;
+  selectedDriveLicence: driveLicences| undefined;
   @Output() toggle = new EventEmitter<void>();
   emailRegex: any = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
   value: string | undefined;
 
   ngOnInit(): void {
+    this.driveLicences = [
+      { name: 'A', code: 'a' },
+      { name: 'B', code: 'b' },
+      { name: 'C', code: 'c' },
+      { name: 'C1', code: 'c1' },
+      { name: 'C1E', code: 'c1e' },
+      { name: 'CE', code: 'ce' },
+      { name: 'D', code: 'd' },
+      { name: 'D1', code: 'd1' },
+      { name: 'D1E', code: 'd1e' },
+      { name: 'DE', code: 'de' }
+      
+  ];
   }
 
   onToggle() {
@@ -45,8 +65,8 @@ export class RegisterComponent implements OnInit {
         Validators.pattern(this.emailRegex),
       ])
     ),
-    date: new FormControl(''),
-    driveLicense: new FormControl(''),
+    date: new FormControl<Date | null>(null, Validators.required),
+    driveLicence: new FormControl<Date | null>(null, Validators.required),
     expiringLicenseDate: new FormControl(''),
     ragioneSociale: new FormControl(''),
     password: new FormControl(''),
--- src/primeng.module.ts
+++ src/primeng.module.ts
@@ -3,8 +3,9 @@ import { NgModule } from '@angular/core';
 import { ButtonModule } from 'primeng/button';
 import { CalendarModule } from 'primeng/calendar';
 import { InputTextModule } from 'primeng/inputtext';
+import { DropdownModule } from 'primeng/dropdown';
 
 @NgModule({
-  exports: [ButtonModule, CalendarModule ,InputTextModule],
+  exports: [ButtonModule, CalendarModule ,InputTextModule, DropdownModule],
 })
 export class PrimengModule {}
