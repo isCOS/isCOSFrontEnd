e706b985a5a4d7a936d7371dbc517ead180df9b8
FlavioOlivieri <flavio.olivieri2002@gmail.com>
2023-10-19 19:11:49 +0200

    box email in login box problem

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,6 +1,7 @@
 import { Component} from '@angular/core';
 import { Output, EventEmitter } from '@angular/core';
 import { trigger, state, style, animate, transition } from '@angular/animations';
+import { FormControl, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -22,10 +23,28 @@ export class LoginComponent {
   showBox: boolean = true;
  
   @Output() toggle = new EventEmitter<void>();
+  emailRegex: any = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
+  value: string | undefined;
 
   onToggle() {
     this.toggle.emit();
     this.showBox = !this.showBox;
+  }
+  constructor(private fb: FormBuilder) { }
+  form = this.fb.group({
+    email: new FormControl(
+      '',
+      Validators.compose([
+        Validators.required,
+        Validators.pattern(this.emailRegex),
+      ])
+    ),
+    password: new FormControl(''),
+  });
+
+  sendLoginRequest() {
+    console.log(this.form.value);
+    this.form.reset();
   }
 
 }
