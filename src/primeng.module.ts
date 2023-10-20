7f04cc6f44c3d0d310a522df0ce0facc07dd98c7
FlavioOlivieri <flavio.olivieri2002@gmail.com>
2023-10-20 09:15:29 +0200

    login button disabled

--- src/primeng.module.ts
+++ src/primeng.module.ts
@@ -4,8 +4,9 @@ import { ButtonModule } from 'primeng/button';
 import { CalendarModule } from 'primeng/calendar';
 import { InputTextModule } from 'primeng/inputtext';
 import { DropdownModule } from 'primeng/dropdown';
+import { SplitterModule } from 'primeng/splitter';
 
 @NgModule({
-  exports: [ButtonModule, CalendarModule ,InputTextModule, DropdownModule],
+  exports: [ButtonModule, CalendarModule ,InputTextModule, DropdownModule, SplitterModule],
 })
 export class PrimengModule {}
