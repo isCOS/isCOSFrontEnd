b1ec93751d39e182e1ce7e282baec8992ce62e7e
FrancescoUmberto <umbertocarolini4112@gmail.com>
2023-11-20 15:54:18 +0100

    no success

--- src/app/gestione/gestioneAccount/gestioneAccount.component.ts
+++ src/app/gestione/gestioneAccount/gestioneAccount.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { Output, EventEmitter } from '@angular/core';
 import { userService } from '../../service/user.service';
 import { MessageService } from 'primeng/api';
+import { HttpClient } from '@angular/common/http';
+import { ReactiveFormsModule } from '@angular/forms';
 
 interface driveLicences {
   name: string;
@@ -12,22 +14,25 @@ interface driveLicences {
   templateUrl: './gestioneAccount.component.html',
   styleUrls: ['./gestioneAccount.component.scss'],
 })
-export class GestioneAccountComponent implements OnInit{
+export class GestioneAccountComponent implements OnInit {
   showContinueButton = false;
   driveLicences: driveLicences[] | undefined;
   selectedDriveLicence: driveLicences | undefined;
   @Output() toggle = new EventEmitter<void>();
   emailRegex: any = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
-  value: string | undefined;
+  value!: object;
   user: any;
   userEmail: any;
+  jsonUser: any;
+  stringifiedUser: any;
   clonedUser: { [s: string]: any } = {};
-  userDataArray: any;
+
   constructor(
     private userService: userService,
-    private messageService: MessageService
+    private messageService: MessageService,
   ) {}
 
+  // .ts file
   ngOnInit(): void {
     this.driveLicences = [
       { name: 'B' },
@@ -40,26 +45,48 @@ export class GestioneAccountComponent implements OnInit{
       { name: 'D1E' },
       { name: 'DE' },
     ];
-    this.userService.GetUser(sessionStorage.getItem('email')).subscribe((res) => {
-      this.user = res;
-      console.log('User: ',this.user);
-    });
-    return this.user;
+    this.userService
+      .GetUser(sessionStorage.getItem('email'))
+      .subscribe((res) => {
+        this.user = res;
+        console.log('User: ', this.user.data);
+        this.jsonUser = JSON.stringify(this.user.data);
+        this.jsonUser = {
+          dateBirth: this.user?.data.dateBirth,
+          drivingLicenseDeadLine: this.user?.data.drivingLicense?.deadLine,
+          drivingLicenseType: this.user?.data.drivingLicense?.type,
+          email: this.user?.data.email,
+          name: this.user?.data.name,
+          surname: this.user?.data.surname,
+          token: 'default',
+        };
+        console.log('UserSTRING: ', this.jsonUser);
+        return this.user;
+      });
   }
 
   loadUserData() {
-    this.userService.GetUser(sessionStorage.getItem('email')).subscribe((res) => {
-      this.user = res;
-      console.log('User: ',this.user);
-    });
+    this.userService
+      .GetUser(sessionStorage.getItem('email'))
+      .subscribe((res) => {
+        this.user = res;
+        console.log('User: ', this.user);
+      });
   }
-  onRowEditInit(user: any) {
-    this.clonedUser[user?.data.email as string] = { ...user?.data };
+
+  onRowEditInit() {
+    console.log(`Email: ${this.jsonUser.email}`);
+    this.clonedUser[this.jsonUser?.email as string] = { ... this.jsonUser};
+    console.log('ClonedUser: ', this.clonedUser);
   }
 
   onRowEditSave(user: any) {
-    this.userService.EditUser(user).subscribe(res => {
-      this.messageService.add({ severity: 'success', summary: 'Success', detail: 'Subscription Updated' });
+    this.userService.EditUser(user).subscribe((res) => {
+      this.messageService.add({
+        severity: 'success',
+        summary: 'Success',
+        detail: 'Subscription Updated',
+      });
     });
     this.loadUserData();
   }
